756d3e06e54dea8d8c62f1edc3d853096b634b90
Żaneta <kul.zaneta@gmail.com>
2020-06-15 11:11:34 +0200

    city form added

--- src/app/components/public/add-city/add-city.component.ts
+++ src/app/components/public/add-city/add-city.component.ts
@@ -1,8 +1,9 @@
 import { Store } from '@ngxs/store';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { JapaneseCity } from '../state/japanese-city.actions';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-city',
@@ -10,56 +11,72 @@ import { JapaneseCity } from '../state/japanese-city.actions';
   styleUrls: ['./add-city.component.sass']
 })
 export class AddCityComponent implements OnInit {
-  cityForm = new FormGroup({}) 
-  cityFields: FormlyFieldConfig[] = [
-    {
-      key: 'name',
-      type: 'input',
-      templateOptions: {
-        label: 'name',
-        placeholder: 'name of city',
-        required: true
-      }
-    },
-    {
-      key: 'population',
-      type: 'input',
-      templateOptions: {
-        label: 'population'
-      }
-    },
-    {
-      key: 'area',
-      type: 'input',
-      templateOptions: {
-        label: 'area'
-      }
-    },
-    {
-      key: 'details',
-      type: 'textarea',
-      templateOptions: {
-        label: 'details of city',
-        placeholder: 'details of city',
-        required: true
-      }
-    }
-  ]
+  cityId: number
+  cityForm = new FormGroup({})
 
-  constructor(public store: Store, public formBuilder: FormBuilder) { }
+  constructor(public store: Store, public formBuilder: FormBuilder, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.createCityForm(null, null, null, null)
+    this.route.params.subscribe(params => {
+      this.cityId = params['id']
+      if (this.cityId) {
+        this.store.dispatch(new JapaneseCity.FetchDetailsAction(this.cityId)).subscribe(response => {
+          const cities = response.japaneseCity.detailsCity
+          this.createCityForm(cities.name, cities.population, cities.area, cities.description)
+        })
+      }
+    })
+
+  }
+
+  createCityForm(name: null, population: null, area: null, description: null) {
     this.cityForm = this.formBuilder.group({
-      name: [null, Validators.required], 
-      population: null,
-      area: null,
-      details: [null, Validators.required]
+      name: [name, Validators.required],
+      population: population,
+      area: area,
+      details: [description, Validators.required],
+      attraction: this.formBuilder.array([this.createNewParameter()])
     })
   }
 
-  addCity() {
-    this.store.dispatch(new JapaneseCity.AddAction({name: this.cityForm.value.name, population: this.cityForm.value.population,
-       area: this.cityForm.value.area, description: this.cityForm.value.details}))
+  createNewParameter() {
+    return this.formBuilder.group({
+      name: [null, Validators.required],
+      url: [null, Validators.required]
+    })
   }
+
+  getControls() {
+    return (this.cityForm.controls['attraction'] as FormArray).controls
+  }
+
+  addNewAttraction() {
+    const newAttraction = (this.cityForm.controls['attraction'] as FormArray).controls
+    newAttraction.push(this.createNewParameter())
+  }
+
+  removeAttraction(index: number) {
+    const removeAttraction = (this.cityForm.controls['attraction'] as FormArray)
+    removeAttraction.removeAt(index)
+  }
+
+  updateCity() {
+    this.store.dispatch(new JapaneseCity.EditAction({
+      name: this.cityForm.value.name, population: this.cityForm.value.population, area: this.cityForm.value.area, description: this.cityForm.value.details, id: this.cityId
+    }))
+
+  }
+
+  addCity() {
+    this.store.dispatch(new JapaneseCity.AddAction({
+      name: this.cityForm.value.name, population: this.cityForm.value.population,
+      area: this.cityForm.value.area, description: this.cityForm.value.details,
+      attractions: this.cityForm.value.attraction
+    }))
+    this.cityForm.reset()
+  }
+
+
 
 }
